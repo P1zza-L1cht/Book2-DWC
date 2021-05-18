206b8165b65a7f9d9a099dc842e90855592e5fe9
P1zza-L1cht <haruki21corder@gmail.com>
2021-05-18 02:42:17 +0000

    [add]update,delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @book = current_user.id
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.find(@user.id)
+    @books = @user.books
   end
 
   def edit
