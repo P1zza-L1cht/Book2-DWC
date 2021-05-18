33e658268b2cf97100f77d5dc43a934c333af64d
P1zza-L1cht <haruki21corder@gmail.com>
2021-05-18 05:02:19 +0000

    [add]update機能, レイアアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,9 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
   def index
     @books = Book.all
     @book = Book.new
@@ -13,7 +18,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
+    @user = @book.user
+    @new = Book.new
   end
 
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4e3a01a3783d74015a0030fa27b899b455b82f6474f0524001b9359bd432a909b2ed7c096f2eab9c4bc5b9d104d9dfd72b317b62c66b4a4fc41836b502b3acde'
